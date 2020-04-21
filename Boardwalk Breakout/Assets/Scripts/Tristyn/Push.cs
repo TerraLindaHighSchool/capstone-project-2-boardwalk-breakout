d52f5858315b3ad0ee522d6daf8b81e849c6fd87
WooziLover <oohty15@gmail.com>
2020-04-20 20:26:21 -0700

    Worked on Wait and Push Script
    For the wait, I decided to pause on that since I want to finish push and pull, so I can work on how wait deals with push/pull.
    
    Also for the push, I'm debating whether to increase the mass or increase the friction.

---
+++ Boardwalk Breakout/Assets/Scripts/Tristyn/Push.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Push : MonoBehaviour
+{
+    /*
+     *The object has to be a heavy mass if it has to take multiple plushies to move it
+     * Or Increase the ground friction
+     */
+    Rigidbody rigidbody;
+    float pushPower = 5.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            rigidbody.AddForce(Vector3.left * pushPower);
+        }
+    }
+}
