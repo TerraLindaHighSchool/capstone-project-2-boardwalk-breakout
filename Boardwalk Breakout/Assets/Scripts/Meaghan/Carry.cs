2f907c9c73250b222918a71813e94ed69b6ee46c
Meaghan Boykin <meaghanb101@gmail.com>
2020-05-01 00:56:54 -0700

    Push script

--- Boardwalk Breakout/Assets/Scripts/Meaghan/Carry.cs
+++ Boardwalk Breakout/Assets/Scripts/Meaghan/Carry.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class Carry : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int numPlushReq;
+    public GameObject objectCarried;
+
+    private Rigidbody carryRB;
+    private Rigidbody plushieRB;
+    private int numPlush;
+    private GameObject firstPlushie;
+
+    private void Start()
     {
-        
+        carryRB = objectCarried.GetComponent<Rigidbody>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Plushie")
+        {
+            if (numPlush == 0)
+            {
+                firstPlushie = other.gameObject;
+                plushieRB = other.GetComponent<Rigidbody>();
+            }
+            numPlush++;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Plushie")
+        {
+            numPlush--;
+            if (numPlush == 0)
+            {
+                firstPlushie = null;
+                plushieRB = null;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (numPlush >= numPlushReq)
+        {
+            carryRB.position = plushieRB.position + new Vector3(0, firstPlushie.transform.lossyScale.y * 1.5f, 0);
+            objectCarried.transform.parent = firstPlushie.transform;
+        }
     }
 }
