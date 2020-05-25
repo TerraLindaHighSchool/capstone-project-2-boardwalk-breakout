e7a59a18ed813cda9b85b9858ae9d1ce8f63b0b6
WooziLover <oohty15@gmail.com>
2020-05-25 01:20:36 -0700

    Did many things. Some are in progress but most finished
    Animation
    Menus

---
+++ Boardwalk Breakout/Assets/Scripts/Final Scripts/Menus/GameEnding.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnding : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject gameEndingUI;
+    public float plushieInventory = 3;
+
+    bool isPlayerAtExit;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (plushieInventory == 3)
+        {
+            if (other.gameObject == player)
+            {
+                isPlayerAtExit = true;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (isPlayerAtExit)
+        {
+            endLevel();
+        }    
+    }
+
+    void endLevel()
+    {
+        gameEndingUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void playAgain()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
---
+++ Boardwalk Breakout/Assets/Scripts/Final Scripts/Menus/Lose.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lose : MonoBehaviour
+{
+    public GameObject loseUI;
+    public float inventory = 3;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inventory == inventory - 1)
+        {
+            loseUI.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
