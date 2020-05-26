61a66b6ce4b524c2127ac7642eb2c55ce8458b7e
WooziLover <oohty15@gmail.com>
2020-05-25 22:28:10 -0700

    added stuff

---
+++ Boardwalk Breakout/Assets/Scripts/Final Scripts/MainMusic.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMusic : MonoBehaviour
+{
+    public AudioSource introMusic;
+    public AudioSource mainMusic;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        introMusic.Play();
+        mainMusic.Play();
+    }
+
+    
+}
